0f505b0a556528c77c25c50a2bc1a0522e726422
Nicholas Mutale <nicomutale@gmail.com>
2022-07-23 20:10:18 +0200

    Install rack cors

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,12 @@ module HelloRailsBackEnd
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    Rails.application.config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:post]
+        resource '*', headers: :any, methods: [:get, :delete, :put, :patch, :options]
+      end
+    end
   end
 end
